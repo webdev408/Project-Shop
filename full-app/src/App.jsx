39139d201f3dea2510b048e7cfb35381586a0db3
Yagya <prolific2476@gmail.com>
2023-07-25 14:22:44 -0400

    started backend to the application

--- full-app/src/App.jsx
+++ full-app/src/App.jsx
@@ -1,13 +1,24 @@
 
-import './index.css'
+import Footer from './components/Footer';
+import { Outlet} from 'react-router-dom';
+import Header from './components/Header';
+import './assets/index.css';
+import './assets/bootstrap.custom.css'
+import { Container } from 'react-bootstrap';
 
 function App() {
 
   return (
-      <div>
-        <h1>Welcome to ProShop</h1>
-      </div>
-    )
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- full-app/src/main.jsx
+++ full-app/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+// import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App.jsx'
-import './index.css'
+import './assets/bootstrap.custom.css'
+import './assets/index.css'
+import HomeScreen from './screens/HomeScreen.jsx'
+import ProductScreen from './screens/ProductScreen.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
+
